ab442d3fc0bc42350460a7ac8136b5fea334874a
Duda Sousa <mariaeduarda006duda@outlook.com>
2024-09-18 09:14:32 -0300

    Add files via upload

---
+++ git_31/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+<h1>Versionamenato com GIT/GITHUB</h1>
+<h2>Aula 11/09</h2>
+<p>Executando git commit</p>
+<a href="pagina1.php">Ir para página 1</a>
+</body>
+</html>
